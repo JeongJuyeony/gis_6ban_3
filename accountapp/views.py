56fce6a23402cce2b4b0364f62edd941507cf108
jeong <hi_coffeeci@likelion.org>
2021-10-11 02:10:06 +0900

    10/11 6ban_3 commit 5

--- accountapp/views.py
+++ accountapp/views.py
@@ -7,11 +7,19 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
+from accountapp.models import HelloWorld
+
 
 def hello_world(request):
 
     if request.method == "POST":
-        return render(request, 'accountapp/hello_world.html', context={'text': 'POST METHOD!!'})
+
+        temp = request.POST.get('hello_world_input')
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})
     else:
         return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!!'})
 
